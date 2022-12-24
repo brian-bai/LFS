e6d2700f0f82bcb05df11b2d944252b3d86325f5
Brian Bai <baitq@outlook.com>
2022-12-24 18:05:06 +0800

    init setup

---
+++ step1_prepare_system.sh
@@ -0,0 +1,15 @@
+echo " use this script to pass version check scripts"
+echo " in linux from scratch version 11.2 chapter 2.2 Host System Requirement"
+echo " Test on ubuntu22.04, adapt this script as your step0 version check result required"
+
+if [ "$EUID" -ne 0 ]
+  then echo "------ Need to run as root with sudo"
+  exit
+fi
+rm /bin/sh
+ln -s /bin/bash /bin/sh
+apt install gawk
+apt install bison
+apt install make
+apt install texinfo
+apt-get install build-essential
---
+++ step3-prepare.sh
@@ -0,0 +1,3 @@
+echo "Prepare the LFS build root directory, the following command need to be executed manually"
+echo "export DIST_ROOT=/home/brian/workspace/LFS"
+echo "export LFS=\$DIST_ROOT/build_env/build_root"
